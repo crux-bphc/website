53673db88acb82b18d77fe8da96eb0d53451c4d0
soumitradev <soumitra.shewale.dev@gmail.com>
2024-02-06 23:56:08 +0530

    build: add compression plugin for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import compress from "astro-compress";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), compress()]
 });
