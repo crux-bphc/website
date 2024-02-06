bb36b62ea1c9aabfe58573978c349e6727b94c62
soumitradev <soumitra.shewale.dev@gmail.com>
2024-02-06 23:10:15 +0530

    style: format existing config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
-import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
