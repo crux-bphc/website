73ea219000e7dbe7db50065e85f31193b9fffb0e
soumitradev <soumitra.shewale.dev@gmail.com>
2024-02-18 15:53:41 +0530

    style: format astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), compress()]
+  integrations: [tailwind(), svelte(), compress()],
 });
