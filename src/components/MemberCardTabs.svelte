69c4cff462068997c2fd9adec8bcafdc87fefeb2
soumitradev <soumitra.shewale.dev@gmail.com>
2024-04-23 16:03:27 +0530

    fix: make MemberCardTabs more responsive for actual page usecase

--- src/components/MemberCardTabs.svelte
+++ src/components/MemberCardTabs.svelte
@@ -2,13 +2,13 @@
   import { Tabs } from "bits-ui";
 </script>
 
-<div class="w-full pt-6 lg:w-9/12">
+<div class="w-full pt-6 lg:w-11/12 xl:w-9/12">
   <Tabs.Root value="members" class="p-3">
     <Tabs.List
       class="flex w-full flex-col justify-end gap-2 rounded-md px-1 text-base font-bold lg:flex-row lg:text-lg"
     >
       <span
-        class="w-full pl-3 pr-1 text-3xl font-extrabold text-foreground md:text-4xl"
+        class="w-full pl-1 pr-1 text-2xl font-extrabold text-foreground md:text-4xl lg:pl-3 lg:font-black"
       >
         Members & Alumni</span
       >
@@ -29,14 +29,14 @@
     </Tabs.List>
     <Tabs.Content value="members" class="pt-1 lg:pt-2">
       <div
-        class="grid grid-cols-1 gap-2 p-2 pb-1 lg:grid-cols-3 lg:gap-4 lg:p-4"
+        class="grid grid-cols-1 gap-2 p-2 pb-1 lg:grid-cols-3 lg:gap-4 lg:p-4 xl:gap-8"
       >
         <slot name="members" />
       </div>
     </Tabs.Content>
     <Tabs.Content value="alumni" class="pt-1 lg:pt-2">
       <div
-        class="grid grid-cols-1 gap-2 p-2 pb-1 lg:grid-cols-3 lg:gap-4 lg:p-4"
+        class="grid grid-cols-1 gap-2 p-2 pb-1 lg:grid-cols-3 lg:gap-4 lg:p-4 xl:gap-8"
       >
         <slot name="alumni" />
       </div>
