e097d5782b40bc99fbabb43eee7b0f7ec469954a
soumitradev <soumitra.shewale.dev@gmail.com>
2024-04-23 20:36:45 +0530

    fix: add links to menu component and switch to transitions instead of opacity to avoid random clicks

--- src/components/LandingPageMenu.svelte
+++ src/components/LandingPageMenu.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { cubicOut } from "svelte/easing";
+  import { fade } from "svelte/transition";
+
   let opened = false;
 
   const toggle = () => {
@@ -12,10 +15,31 @@
   class="material-symbols-rounded font-2xl cursor-pointer select-none font-bold opacity-80"
   on:click={toggle}>menu</span
 >
-<div
-  class={`absolute -right-8 ${window.scrollY > 128 ? "top-16" : "-top-36"} flex w-screen select-none flex-col space-y-4 bg-navbar/80 px-4 py-4 text-right text-xl font-bold text-foreground-muted transition-all duration-300 ${opened ? "opacity-100" : "opacity-0"}`}
->
-  <span>Achievements</span>
-  <span>Projects</span>
-  <span>Members & Alumni</span>
-</div>
+{#if opened}
+  <div
+    class={`absolute -right-8 ${window.scrollY > 128 ? "top-16" : "-top-36"} flex w-screen select-none flex-col space-y-4 bg-navbar/80 px-4 py-4 text-right text-xl font-bold text-foreground-muted transition-all duration-300`}
+    transition:fade={{
+      duration: 400,
+      easing: cubicOut,
+    }}
+  >
+    <a
+      class="text-foreground-muted transition-all duration-200 hover:text-purple hover:underline"
+      href="/achievements"
+    >
+      Achievements
+    </a>
+    <a
+      class="text-foreground-muted transition-all duration-200 hover:text-light-blue hover:underline"
+      href="/projects"
+    >
+      Projects
+    </a>
+    <a
+      class="text-foreground-muted transition-all duration-200 hover:text-orange hover:underline"
+      href="/members"
+    >
+      Members & Alumni
+    </a>
+  </div>
+{/if}
